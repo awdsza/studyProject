968d0c01de552da1be0b2b641ffbe21fe63b1a8a
ljh <ljh0807@noaa.co.kr>
2018-05-25 11:48:23 +0900

    Update

--- toy/src/test/java/com/toy/ToyApplicationTests.java
+++ toy/src/test/java/com/toy/ToyApplicationTests.java
@@ -1,7 +1,5 @@
 package com.toy;
 
-import java.util.HashMap;
-
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,11 +7,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.toy.code.dto.CodeDto;
-import com.toy.code.mapper.CodeMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -34,12 +31,4 @@ public class ToyApplicationTests {
 	        System.out.println("sqlSession : "+sqlSession);
 	        
 	    }
-	    @Test
-	    public void pager(){
-	    	int pageNum = 2;
-	    	int pageSize = 5;
-			int startPage = ((pageNum-1) * pageSize) + 1;
-			int endPage = pageNum* pageSize;
-			System.out.println("startPage :: " + startPage +" endPage :: " + endPage);
-		}
 }
