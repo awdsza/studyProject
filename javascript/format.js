9d3fee91533603981a449815c3f33054365df6da
awdsza <37355034+awdsza@users.noreply.github.com>
2019-04-15 22:06:34 +0900

    Update format.js

--- javascript/format.js
+++ javascript/format.js
@@ -1,10 +1,10 @@
-
 /* @ parameter 
  * @ example "{0}{1}{0}".format('A','B','C'); 
  * @ result -> ABC
 */
 String.prototype.format = function(){
     var result = this ;
+    //호출로 넘긴 인자(arguments)를 for문으로 돌리면서 replace를 시킨다.
     for(var i=0;i<arguments.length;i++){
        var patExp = new RegExp("\\{"+i+"\\}","gm");
        result = result.replace(patExp,arguments[i]);
